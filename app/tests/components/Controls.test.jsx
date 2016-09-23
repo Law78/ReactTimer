df88e55ccccdfa9455eb672a62e524aebe1a1040
Lorenzo <Lorenzo@MacBook-Pro-di-Lorenzo.local>
2016-09-23 12:48:54 +0200

    Wire up controls component

--- app/tests/components/Controls.test.jsx
+++ app/tests/components/Controls.test.jsx
@@ -12,45 +12,64 @@ describe('Controls', () => {
   });
 
   describe('render', () => {
-    it('should render pause when started', () =>{
-      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+    var spy = expect.createSpy();
+    it('should render pause when started and pass value paused when clicked', () =>{
+
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started" onStatusChange={spy}/>);
 
       expect(controls.refs.pausebutton).toExist();
+      TestUtils.Simulate.click(controls.refs.pausebutton);
+      expect(spy).toHaveBeenCalledWith('paused')
     });
-    it('should render pause when started', () =>{
-      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+    it('should render pause when started and pass value paused when clicked', () =>{
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started" onStatusChange={spy}/>);
       var $el = $(ReactDOM.findDOMNode(controls));
       var $pauseButton = $el.find('button:contains(Pause)');
 
       expect($pauseButton.length).toBe(1);
+      TestUtils.Simulate.click(controls.refs.pausebutton);
+      expect(spy).toHaveBeenCalledWith('paused')
     });
-    it('should render pause when started', () =>{
-      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+    it('should render pause when started and pass value paused when clicked', () =>{
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started" onStatusChange={spy}/>);
       var elems = TestUtils.scryRenderedDOMComponentsWithTag(controls, "button");
 
       expect( (ReactDOM.findDOMNode(elems[0])).textContent).toEqual("Pause");
+
+      TestUtils.Simulate.click(controls.refs.pausebutton);
+      expect(spy).toHaveBeenCalledWith('paused')
     });
-    it('should render start when paused', () =>{
-      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
+    it('should render start when paused and pass value started when clicked', () =>{
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused" onStatusChange={spy}/>);
       var $el = $(ReactDOM.findDOMNode(controls));
       var $startButton = $el.find('button:contains(Start)');
 
       expect($startButton.length).toBe(1);
+
     });
-    it('should render start and clear when paused', () =>{
-      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused"/>);
+    it('should render start and clear when paused and pass value started or stopped when clicked', () =>{
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="paused" onStatusChange={spy}/>);
       var elems = TestUtils.scryRenderedDOMComponentsWithTag(controls, "button");
       expect( (ReactDOM.findDOMNode(elems[0])).textContent).toEqual("Start");
       expect( (ReactDOM.findDOMNode(elems[1])).textContent).toEqual("Clear");
 
+      TestUtils.Simulate.click(elems[0]);
+      expect(spy).toHaveBeenCalledWith('started')
+      TestUtils.Simulate.click(elems[1]);
+      expect(spy).toHaveBeenCalledWith('stopped')
+
     });
 
-    it('should render pause and clear when started', () =>{
-      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started"/>);
+    it('should render pause and clear when started and pass value paused or stopped when clicked', () =>{
+      var controls = TestUtils.renderIntoDocument(<Controls countdownStatus="started" onStatusChange={spy}/>);
       var elems = TestUtils.scryRenderedDOMComponentsWithTag(controls, "button");
       expect( (ReactDOM.findDOMNode(elems[0])).textContent).toEqual("Pause");
       expect( (ReactDOM.findDOMNode(elems[1])).textContent).toEqual("Clear");
 
+      TestUtils.Simulate.click(elems[0]);
+      expect(spy).toHaveBeenCalledWith('paused')
+      TestUtils.Simulate.click(elems[1]);
+      expect(spy).toHaveBeenCalledWith('stopped')
     });
   });
 });
